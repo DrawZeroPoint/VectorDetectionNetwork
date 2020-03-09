395b1021de90e8f5e0ab8acedfea0982f113aeb8
YIGAOYI <960389872@qq.com>
2020-03-09 09:59:57 +0800

    to default

--- eval.py
+++ eval.py
@@ -9,15 +9,15 @@ import modules.vdn as vdn
 
 class Test(unittest.TestCase):
 
-    # def test_eval_default(self):
-    #     """
-    #     """
-    #     vdn_instance = vdn.VectorDetectionNetwork(backbone='resnet50')
-    #     vdn_instance.eval()
-
-    def test_eval_resnet34(self):
-        vdn_instance = vdn.VectorDetectionNetwork(backbone='resnet34')
+    def test_eval_default(self):
+        """
+        """
+        vdn_instance = vdn.VectorDetectionNetwork(backbone='resnet50')
         vdn_instance.eval()
+
+    # def test_eval_resnet34(self):
+    #     vdn_instance = vdn.VectorDetectionNetwork(backbone='resnet34')
+    #     vdn_instance.eval()
 
 
 if __name__ == '__main__':
